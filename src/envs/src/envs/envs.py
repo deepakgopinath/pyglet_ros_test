7abc7bd0ac79f148b9b5da489f3cd39183f62133
Deepak Gopinath <musicmagic@gmail.com>
2020-01-08 16:44:42 -0600

    Joystick parsing. Map velocities to robot

--- src/envs/src/envs/envs.py
+++ src/envs/src/envs/envs.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
-from Box2D import (b2EdgeShape, b2FixtureDef, b2PolygonShape, b2Random, b2CircleShape)
+from Box2D import (b2EdgeShape, b2FixtureDef, b2PolygonShape, b2Random, b2CircleShape, b2Vec2)
 import rospy
 from backends.framework import (Framework, Keys)
 from IPython import embed
 
 class Robot(object):
-	def __init__(self, world, position, radius):
-		self.robot = world.CreateKinematicBody(
-            position=position,
-            shapes=[b2CircleShape(pos=position, radius=radius)]
-		)
-		
+	def __init__(self, world, position, radius=3, type='kinematic'):
+		if type == 'kinematic':
+			self.robot = world.CreateKinematicBody(
+	            position=position,
+	            shapes=[b2CircleShape(pos=position, radius=radius)]
+			)
+
+		#TODO add else with creation of dynamic body
+
 
 	def update(self, velocity):
-		pass
+		self.robot.linearVelocity = velocity
 
 class Goal(object):
 	def __init__(self, world, position, radius):
@@ -31,28 +34,30 @@ class SimpleHA2D(Framework):
 	description = '2D environment for human autonomy point robots'
 	def __init__(self, num_goals):
 		super(SimpleHA2D, self).__init__()
-		self.world.gravity = (0,0) #So that this is a flat 2D world with no grvity. 
+		self.world.gravity = (0,0) #So that this is a flat 2D world with no grvity.
 		self.num_goals = num_goals
 		self.autonomy_goals = []
 		self.human_goals = []
-		self.autonomy_velocity = 0.0
-		self.human_velocity = 0.0
+		self.autonomy_velocity = b2Vec2(0.0, 0.0)
+		self.human_velocity = b2Vec2(0.0, 0.0)
 
 		ground = self.world.CreateStaticBody(
             position=(0, 0),
             shapes=[b2EdgeShape(vertices=[(0, 35), (0, -35)])]
 		)
-		
-		self.autonomy_robot = Robot(self.world, position=(5,9))
-		self.human_robot = Robot(self.world, position=(-5,9))
+
+		self.autonomy_robot = Robot(self.world, position=(5,9), radius=1)
+		self.human_robot = Robot(self.world, position=(-5,9), radius=1)
         # self.body = self.world.CreateDynamicBody(position=(10,0))
 
 
 	def update_human_velocity(self, human_velocity):
-		self.human_velocity = human_velocity
+		self.human_velocity.x = human_velocity.velocity.data[0]
+		self.human_velocity.y = human_velocity.velocity.data[1]
 
 	def update_autonomy_velocity(self, autonomy_velocity):
-		self.autonomy_velocity = autonomy_velocity
+		self.autonomy_velocity.x = autonomy_velocity.velocity.data[0]
+		self.autonomy_velocity.y = autonomy_velocity.velocity.data[1]
 
 	def Step(self, settings):
 		self.autonomy_robot.update(self.autonomy_velocity)
@@ -69,4 +74,3 @@ class ComplexHA2D(Framework):
 
 	def Step(self):
 		pass
-
--- src/simple_ha2d/scripts/system.py
+++ src/simple_ha2d/scripts/system.py
@@ -4,7 +4,10 @@
 import rospy
 from sensor_msgs.msg import Joy
 from envs.envs import SimpleHA2D
+from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import MultiArrayDimension
 from simple_ha2d.msg import CartVelCmd
+from IPython import embed
 import numpy as np
 
 class Simulator(object):
@@ -16,28 +19,27 @@ class Simulator(object):
 		rospy.Subscriber('/joy', Joy, self.joy_callback)
 		self.env = SimpleHA2D(2)
 		self.dim = dim
+
+		self.user_vel = CartVelCmd()
+		_dim = [MultiArrayDimension()]
+		_dim[0].label = 'cartesian_velocity'
+		_dim[0].size = 2
+		_dim[0].stride = 2
+		self.user_vel.velocity.layout.dim = _dim
+		self.user_vel.velocity.data = np.zeros(self.dim)
+		self.user_vel.header.stamp = rospy.Time.now()
+		self.user_vel.header.frame_id = 'human_control'
+
+
 		if rospy.has_param('max_cart_vel'):
 			self._max_cart_vel = np.array(rospy.get_param('max_cart_vel'))
 		else:
-			self._max_cart_vel = 0.25*np.ones(self.dim)
+			self._max_cart_vel = 10*np.ones(self.dim)
 			rospy.logwarn('No rosparam for max_cart_vel found...Defaulting to max linear velocity of 50 cm/s and max rotational velocity of 50 degrees/s')
 
 		r = rospy.Rate(10)
 		isStart = True
 		self.env.run()
-		
-		# while not rospy.is_shutdown():
-		# 	print "H"
-		# 	#update human robot and autonomy actions
-
-		# 	#simulate physics
-
-		# 	#render to screen
-
-		# 	if isStart:
-
-		# 		isStart = False
-		# 	r.sleep()
 
 	def joy_callback(self, msg):
 		_axes = np.array(msg.axes)
@@ -45,12 +47,14 @@ class Simulator(object):
 			self.user_vel.velocity.data[i] = 0.0
 
 		self.user_vel.velocity.data[0] = -_axes[0] * self._max_cart_vel[0]
-		self.user_vel.velocity.data[1] = -_axes[1] * self._max_cart_vel[1]
+		self.user_vel.velocity.data[1] = _axes[1] * self._max_cart_vel[1]
 		self.user_vel.header.stamp = rospy.Time.now()
 		self.env.update_human_velocity(self.user_vel)
 
 	def shutdown_hook(self):
 		pass
+
 if __name__ == '__main__':
+	#TODO parse arguments for num dimensions etc. To be included in launch file
 	Simulator(2)
 	rospy.spin()
